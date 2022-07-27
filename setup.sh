5a059615760c774f75e50deb77c4b607130161cf
Dhina17 <dhinalogu@gmail.com>
2022-07-27 23:51:58 +0530

    dotfiles: gtk: Install theme from original repo

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ set -e
 # default user dir
 USER_DIR="/home/dhina17"
 GRUB_THEME_DIR="${USER_DIR}/.grub/themes/dracula"
+THEMES_DIR="${USER_DIR}/.themes/dracula-gtk"
 
 # System update
 sudo pacman -Syu --noconfirm
@@ -17,3 +18,6 @@ cp -r .  $USER_DIR
 # Install grub themes
 git clone https://github.com/dracula/grub ${GRUB_THEME_DIR}
 sudo echo GRUB_THEME="${GRUB_THEME_DIR}/dracula/theme.txt" >> /etc/default/grub
+
+# Install GTK themes
+git clone https://github.com/dracula/gtk ${THEMES_DIR}
