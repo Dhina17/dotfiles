6546784c2136779cfe86d2bb659e94cb9bfe33e6
Dhina17 <dhinalogu@gmail.com>
2023-03-06 23:07:07 +0530

    script: Setup i3

--- setup.sh
+++ setup.sh
@@ -10,8 +10,32 @@ WALLS_DIR="${USER_DIR}/wallpapers"
 # System update
 sudo pacman -Syu --noconfirm
 
-# Install git
-sudo pacman -S --noconfirm git
+# Install required packages
+sudo pacman -S --noconfirm base-devel git \
+        xorg xorg-xinit xf86-video-amdgpu mesa \
+        i3-wm i3status \
+        ttf-jetbrains-mono noto-fonts \
+        feh nm-applet dmenu
+
+# Setup i3 with xinit
+sudo echo "exec i3" >> /etc/X11/xinit/xinitrc
+
+# Install yay
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git
+makepkg -si --noconfirm
+cd ..
+rm -rf yay-git
+
+# Install ly (display manager)
+yay -S --noconfirm ly
+
+# Install luke-st (terminal) from my fork
+git clone https://github.com/Dhina17/luke-st
+cd luke-st
+sudo make install
+cd ..
+rm -rf luke-st
 
 # Move all dotfiles to the user dir
 cp -r .  $USER_DIR
